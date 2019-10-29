c85198a32ef1202a48eeaa754026bb8c505baf52
Eddie Morales <eddie@zipcoders-MacBook-Pro.local>
2019-10-29 19:29:11 -0400

    Create Classes Die

--- src/main/java/com/github/zipcodewilmington/Dice.java
+++ src/main/java/com/github/zipcodewilmington/Dice.java
@@ -1,5 +1,11 @@
 package com.github.zipcodewilmington;
 
 public class Dice {
+    private final Integer numberOfTosses;
+    private final Die[] dice;
 
+    public Dice(Integer numberOfDice, Integer numberOfTosses) {
+        this.numberOfTosses = numberOfTosses;
+        this.dice = new Die[numberOfDice];
+    }
 }
