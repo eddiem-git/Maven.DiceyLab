5100059247a581ab8125928a5291e81bf98821fb
Eddie Morales <eddie@zipcoders-MacBook-Pro.local>
2019-10-29 22:13:35 -0400

    Created Test

---
+++ src/test/java/com/github/zipcodewilmington/bintest/ConstructorTest.java
@@ -0,0 +1,5 @@
+package com.github.zipcodewilmington.bintest;
+
+public class ConstructorTest {
+
+}
---
+++ src/test/java/com/github/zipcodewilmington/simulationTest/ConstructorTest.java
@@ -0,0 +1,4 @@
+package com.github.zipcodewilmington.simulationTest;
+
+public class ConstructorTest {
+}
